0d6ee0bf7490aae134425b544ac70dd3219a12b5
Joey Galvin <joeygalvin2134@gmail.com>
2025-03-02 22:00:10 +0000

    added dark style to map

--- frontend/src/compenents/MapComponent.jsx
+++ frontend/src/compenents/MapComponent.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; // Ensure this import is included
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MAPTILER_API_KEY = import.meta.env.VITE_MAPTILER_KEY; // Replace with your actual API key
 
 const MapComponent = () => {
     return (
         <MapContainer
             center={[51.505, -0.09]}
-            zoom={4}
-            style={{ height: '500px', width: '100%' }}
+            zoom={3}
+            style={{ height: '1000px', width: '100%' }}
         >
             <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url={`https://api.maptiler.com/maps/dataviz-dark/{z}/{x}/{y}.png?key=${MAPTILER_API_KEY}`}
+                attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a> contributors'
             />
         </MapContainer>
     );
